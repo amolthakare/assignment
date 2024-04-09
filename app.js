084353f2e0872f861761b15b9be08e2a4efb4579
Amol Thakare <100188653+amolthakare@users.noreply.github.com>
2024-04-09 18:41:21 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const sequelize = require('./database');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 4500;
+
+app.use(express.static('public'));
+app.use('/', routes);
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
