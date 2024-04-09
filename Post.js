be2529935efd0b0bcdc09791a6f4572f61d0aa27
Amol Thakare <100188653+amolthakare@users.noreply.github.com>
2024-04-09 18:42:54 +0530

    Create Post.js

---
+++ Post.js
@@ -0,0 +1,9 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const Post = sequelize.define('Post', {
+  title: DataTypes.STRING,
+  body: DataTypes.TEXT
+});
+
+module.exports = Post;
