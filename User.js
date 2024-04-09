df1e28a1287d3e6971baeb01381bc8290bdf1dbe
Amol Thakare <100188653+amolthakare@users.noreply.github.com>
2024-04-09 18:44:02 +0530

    Create User.js

---
+++ User.js
@@ -0,0 +1,13 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const User = sequelize.define('User', {
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+  phone: DataTypes.STRING,
+  website: DataTypes.STRING,
+  city: DataTypes.STRING,
+  company: DataTypes.STRING
+});
+
+module.exports = User;
