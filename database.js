b507827a649e90ade84a3f419f28308b8e99e809
Amol Thakare <100188653+amolthakare@users.noreply.github.com>
2024-04-10 20:48:11 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('evaluation','avnadmin','AVNS_l76yf8pt_4GmaI2EHAI',{
-  host: 'mysql-2a0788cf-amolthakare631-9302.b.aivencloud.com',
-  dialect: 'mysql'
-})
+const sequelize = new Sequelize('mysql://avnadmin:AVNS_l76yf8pt_4GmaI2EHAI@mysql-2a0788cf-amolthakare631-9302.b.aivencloud.com:23526/evaluation', {
+  dialect: 'mysql',
+  host: 'mysql-2a0788cf-amolthakare631-9302.b.aivencloud.com'
+});
 
 module.exports = sequelize;
