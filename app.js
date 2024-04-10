fd524527080e459f54d051b41ae8c780657d335a
Amol Thakare <100188653+amolthakare@users.noreply.github.com>
2024-04-10 10:02:24 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const sequelize = require('./database');
 const routes = require('./index');
-
+const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT || 5500;
 
+app.use(cors());
 app.use(express.static('public'));
 app.use('/', routes);
 
--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('evaluation','root','Amol@123',{
-  host: 'localhost',
+const sequelize = new Sequelize('evaluation','avnadmin','AVNS_l76yf8pt_4GmaI2EHAI',{
+  host: 'mysql-2a0788cf-amolthakare631-9302.b.aivencloud.com',
   dialect: 'mysql'
 })
 
